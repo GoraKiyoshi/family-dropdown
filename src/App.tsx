033fd34f86be3d432917ff6df3157a824bc008c9
GoraKiyoshi <longtoanbui2002@gmail.com>
2025-04-15 15:55:22 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,11 @@ const danhMucThuoc: ThuocItem[] = [
   { id: 5, tenThuoc: "Arcoxia 20mg" },
   { id: 6, tenThuoc: "Arcoxia 10mg" },
   { id: 7, tenThuoc: "Arcoxia 1000mg" },
+  { id: 8, tenThuoc: "ACUpan 20mg/2ml Inj" },
+  { id: 9, tenThuoc: "Agimol 80mg Sachets" },
+  { id: 10, tenThuoc: "Arcoxia 60mg" },
+  { id: 11, tenThuoc: "Arcoxia 5mg" },
+  { id: 12, tenThuoc: "Arcoxia 20mg" },
 ];
 
 const initialData: DataItem[] = [
